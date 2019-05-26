cc2d11a53345ce968da379c6664044023ce741d0
adBODKAt <adbodkat@mail.ru>
2019-05-26 17:59:23 +0300

    init commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Александр Марков on 26/05/2019.
+//  Copyright © 2019 Александр Марков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
